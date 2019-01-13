7ceb3741851600a7e2756a2cd4df2b9bc55f92cf
mike <mango@live.cn>
2019-01-13 13:26:31 +0800

    updated artifects

--- src/DefaultMutableTreeNode+refresh.kt
+++ src/DefaultMutableTreeNode+refresh.kt
@@ -2,32 +2,30 @@ import java.io.File
 import javax.swing.tree.DefaultMutableTreeNode
 
 fun DefaultMutableTreeNode.refresh(basePath: File) {
-    run {
-        removeAllChildren()
-        basePath.listFiles { file ->
-            file.isDirectory && file.extension == "db"
-        }?.map { databaseFile ->
-            Pair(DefaultMutableTreeNode("Tables"), DefaultMutableTreeNode("Indexes")).apply {
-                databaseFile.listFiles { file ->
-                    file.isFile && (file.extension == "tab")
-                }?.map { tableFile ->
-                    DefaultMutableTreeNode(tableFile.nameWithoutExtension)
-                }?.forEach { tableNode -> first.add(tableNode) }
-                databaseFile.listFiles { file ->
-                    file.isFile && (file.extension == "idx")
-                }?.map { tableFile ->
-                    DefaultMutableTreeNode(tableFile.nameWithoutExtension)
-                }?.forEach { tableNode ->
-                    second.add(tableNode)
-                }
-            }.run {
-                DefaultMutableTreeNode(databaseFile.nameWithoutExtension).apply {
-                    add(first)
-                    add(second)
-                }
+    removeAllChildren()
+    basePath.listFiles { file ->
+        file.isDirectory && file.extension == "db"
+    }?.map { databaseFile ->
+        Pair(DefaultMutableTreeNode("Tables"), DefaultMutableTreeNode("Indexes")).apply {
+            databaseFile.listFiles { file ->
+                file.isFile && (file.extension == "tab")
+            }?.map { tableFile ->
+                DefaultMutableTreeNode(tableFile.nameWithoutExtension)
+            }?.forEach { tableNode -> first.add(tableNode) }
+            databaseFile.listFiles { file ->
+                file.isFile && (file.extension == "idx")
+            }?.map { tableFile ->
+                DefaultMutableTreeNode(tableFile.nameWithoutExtension)
+            }?.forEach { tableNode ->
+                second.add(tableNode)
             }
-        }?.forEach { databaseNode ->
-            add(databaseNode)
+        }.run {
+            DefaultMutableTreeNode(databaseFile.nameWithoutExtension).apply {
+                add(first)
+                add(second)
+            }
         }
+    }?.forEach { databaseNode ->
+        add(databaseNode)
     }
 }
